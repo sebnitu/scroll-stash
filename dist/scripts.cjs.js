ff266c07e736d7d1b9a387c41463b3bc618498f3
sebnitu <nitu.seb@gmail.com>
2020-07-18 14:15:53 -0700

    refactor: revert some changes to init

--- dist/scripts.cjs.js
+++ dist/scripts.cjs.js
@@ -249,6 +249,218 @@ function toNumber(value) {
 
 var lodash_throttle = throttle;
 
+var lodash_isempty = createCommonjsModule(function (module, exports) {
+  var MAX_SAFE_INTEGER = 9007199254740991;
+  var argsTag = '[object Arguments]',
+      funcTag = '[object Function]',
+      genTag = '[object GeneratorFunction]',
+      mapTag = '[object Map]',
+      objectTag = '[object Object]',
+      promiseTag = '[object Promise]',
+      setTag = '[object Set]',
+      weakMapTag = '[object WeakMap]';
+  var dataViewTag = '[object DataView]';
+  var reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
+  var reIsHostCtor = /^\[object .+?Constructor\]$/;
+  var freeGlobal = _typeof_1(commonjsGlobal) == 'object' && commonjsGlobal && commonjsGlobal.Object === Object && commonjsGlobal;
+  var freeSelf = (typeof self === "undefined" ? "undefined" : _typeof_1(self)) == 'object' && self && self.Object === Object && self;
+  var root = freeGlobal || freeSelf || Function('return this')();
+  var freeExports =  exports && !exports.nodeType && exports;
+  var freeModule = freeExports && 'object' == 'object' && module && !module.nodeType && module;
+  var moduleExports = freeModule && freeModule.exports === freeExports;
+
+  function getValue(object, key) {
+    return object == null ? undefined : object[key];
+  }
+
+  function isHostObject(value) {
+    var result = false;
+
+    if (value != null && typeof value.toString != 'function') {
+      try {
+        result = !!(value + '');
+      } catch (e) {}
+    }
+
+    return result;
+  }
+
+  function overArg(func, transform) {
+    return function (arg) {
+      return func(transform(arg));
+    };
+  }
+
+  var funcProto = Function.prototype,
+      objectProto = Object.prototype;
+  var coreJsData = root['__core-js_shared__'];
+
+  var maskSrcKey = function () {
+    var uid = /[^.]+$/.exec(coreJsData && coreJsData.keys && coreJsData.keys.IE_PROTO || '');
+    return uid ? 'Symbol(src)_1.' + uid : '';
+  }();
+
+  var funcToString = funcProto.toString;
+  var hasOwnProperty = objectProto.hasOwnProperty;
+  var objectToString = objectProto.toString;
+  var reIsNative = RegExp('^' + funcToString.call(hasOwnProperty).replace(reRegExpChar, '\\$&').replace(/hasOwnProperty|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?') + '$');
+  var Buffer = moduleExports ? root.Buffer : undefined,
+      propertyIsEnumerable = objectProto.propertyIsEnumerable;
+  var nativeIsBuffer = Buffer ? Buffer.isBuffer : undefined,
+      nativeKeys = overArg(Object.keys, Object);
+  var DataView = getNative(root, 'DataView'),
+      Map = getNative(root, 'Map'),
+      Promise = getNative(root, 'Promise'),
+      Set = getNative(root, 'Set'),
+      WeakMap = getNative(root, 'WeakMap');
+  var nonEnumShadows = !propertyIsEnumerable.call({
+    'valueOf': 1
+  }, 'valueOf');
+  var dataViewCtorString = toSource(DataView),
+      mapCtorString = toSource(Map),
+      promiseCtorString = toSource(Promise),
+      setCtorString = toSource(Set),
+      weakMapCtorString = toSource(WeakMap);
+
+  function baseGetTag(value) {
+    return objectToString.call(value);
+  }
+
+  function baseIsNative(value) {
+    if (!isObject(value) || isMasked(value)) {
+      return false;
+    }
+
+    var pattern = isFunction(value) || isHostObject(value) ? reIsNative : reIsHostCtor;
+    return pattern.test(toSource(value));
+  }
+
+  function getNative(object, key) {
+    var value = getValue(object, key);
+    return baseIsNative(value) ? value : undefined;
+  }
+
+  var getTag = baseGetTag;
+
+  if (DataView && getTag(new DataView(new ArrayBuffer(1))) != dataViewTag || Map && getTag(new Map()) != mapTag || Promise && getTag(Promise.resolve()) != promiseTag || Set && getTag(new Set()) != setTag || WeakMap && getTag(new WeakMap()) != weakMapTag) {
+    getTag = function getTag(value) {
+      var result = objectToString.call(value),
+          Ctor = result == objectTag ? value.constructor : undefined,
+          ctorString = Ctor ? toSource(Ctor) : undefined;
+
+      if (ctorString) {
+        switch (ctorString) {
+          case dataViewCtorString:
+            return dataViewTag;
+
+          case mapCtorString:
+            return mapTag;
+
+          case promiseCtorString:
+            return promiseTag;
+
+          case setCtorString:
+            return setTag;
+
+          case weakMapCtorString:
+            return weakMapTag;
+        }
+      }
+
+      return result;
+    };
+  }
+
+  function isMasked(func) {
+    return !!maskSrcKey && maskSrcKey in func;
+  }
+
+  function isPrototype(value) {
+    var Ctor = value && value.constructor,
+        proto = typeof Ctor == 'function' && Ctor.prototype || objectProto;
+    return value === proto;
+  }
+
+  function toSource(func) {
+    if (func != null) {
+      try {
+        return funcToString.call(func);
+      } catch (e) {}
+
+      try {
+        return func + '';
+      } catch (e) {}
+    }
+
+    return '';
+  }
+
+  function isArguments(value) {
+    return isArrayLikeObject(value) && hasOwnProperty.call(value, 'callee') && (!propertyIsEnumerable.call(value, 'callee') || objectToString.call(value) == argsTag);
+  }
+
+  var isArray = Array.isArray;
+
+  function isArrayLike(value) {
+    return value != null && isLength(value.length) && !isFunction(value);
+  }
+
+  function isArrayLikeObject(value) {
+    return isObjectLike(value) && isArrayLike(value);
+  }
+
+  var isBuffer = nativeIsBuffer || stubFalse;
+
+  function isEmpty(value) {
+    if (isArrayLike(value) && (isArray(value) || typeof value == 'string' || typeof value.splice == 'function' || isBuffer(value) || isArguments(value))) {
+      return !value.length;
+    }
+
+    var tag = getTag(value);
+
+    if (tag == mapTag || tag == setTag) {
+      return !value.size;
+    }
+
+    if (nonEnumShadows || isPrototype(value)) {
+      return !nativeKeys(value).length;
+    }
+
+    for (var key in value) {
+      if (hasOwnProperty.call(value, key)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function isFunction(value) {
+    var tag = isObject(value) ? objectToString.call(value) : '';
+    return tag == funcTag || tag == genTag;
+  }
+
+  function isLength(value) {
+    return typeof value == 'number' && value > -1 && value % 1 == 0 && value <= MAX_SAFE_INTEGER;
+  }
+
+  function isObject(value) {
+    var type = _typeof_1(value);
+
+    return !!value && (type == 'object' || type == 'function');
+  }
+
+  function isObjectLike(value) {
+    return !!value && _typeof_1(value) == 'object';
+  }
+
+  function stubFalse() {
+    return false;
+  }
+
+  module.exports = isEmpty;
+});
+
 var defaults = {
   autoInit: false,
   dataScroll: 'scroll-stash',
@@ -389,7 +601,8 @@ var anchor = {
   show: anchorShow
 };
 
-var stateSave = function stateSave(state, settings) {
+var stateSave = function stateSave(settings) {
+  var state = {};
   var scrolls = document.querySelectorAll("[data-".concat(settings.dataScroll, "]"));
   scrolls.forEach(function (el) {
     var id = el.dataset[camelCase(settings.dataScroll)];
@@ -404,9 +617,9 @@ var stateSave = function stateSave(state, settings) {
   }));
   return state;
 };
-var stateSet = function stateSet(state, settings) {
+var stateSet = function stateSet(settings) {
   if (localStorage.getItem(settings.saveKey)) {
-    state = JSON.parse(localStorage.getItem(settings.saveKey));
+    var state = JSON.parse(localStorage.getItem(settings.saveKey));
     Object.keys(state).forEach(function (key) {
       var item = document.querySelector("[data-".concat(settings.dataScroll, "=\"").concat(key, "\"]"));
       if (item) item.scrollTop = state[key];
@@ -419,7 +632,7 @@ var stateSet = function stateSet(state, settings) {
     }));
     return state;
   } else {
-    return stateSave(state, settings);
+    return {};
   }
 };
 var state = {
@@ -433,11 +646,11 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 var core = (function (options) {
   var api = {};
   api.settings = _objectSpread(_objectSpread({}, defaults), options);
-  api.scrolls = [];
   api.state = {};
+  api.scrolls = [];
 
   var handler = function handler() {
-    return api.state = state.save(api.state, api.settings);
+    return api.state = state.save(api.settings);
   };
 
   var throttleRef = lodash_throttle(handler, api.settings.throttleDelay, {
@@ -447,22 +660,21 @@ var core = (function (options) {
   api.init = function () {
     var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
     if (options) api.settings = _objectSpread(_objectSpread({}, api.settings), options);
+    api.state = state.set(api.settings);
+    api.state = lodash_isempty(api.state) ? state.save(api.settings) : api.state;
     api.scrolls = document.querySelectorAll("[data-".concat(api.settings.dataScroll, "]"));
-    api.state = state.set(api.state, api.settings);
-    setTimeout(function () {
-      api.scrolls.forEach(function (item) {
-        item.addEventListener('scroll', throttleRef, false);
-        anchor.show(item, false, api.settings);
-      });
-    }, api.settings.throttleDelay);
+    api.scrolls.forEach(function (item) {
+      item.addEventListener('scroll', throttleRef, false);
+      anchor.show(item, false, api.settings);
+    });
   };
 
   api.destroy = function () {
     api.scrolls.forEach(function (item) {
       item.removeEventListener('scroll', throttleRef, false);
     });
-    api.scrolls = [];
     api.state = {};
+    api.scrolls = [];
     localStorage.removeItem(api.settings.saveKey);
   };
 
